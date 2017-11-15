c1f4a617fbca2c513a493c91654860f5fbf7dc97
sklsm3 <sklsm3@gmail.com>
2017-11-15 14:59:45 +0900

    update

--- ipsender.go
+++ ipsender.go
@@ -4,35 +4,20 @@ import (
 	ejson "encoding/json"
 	"fmt"
 	"log"
-	//	"math/rand"
 	"net"
 	"net/http"
-	//	"net/rpc"
 	"net/rpc/jsonrpc"
 	"os"
 	"reflect"
 	"time"
 )
 
-/*
-type ipUpdater struct {
-	client *rpc.Client
-}
-*/
-
 type Args struct{ IPS []string }
 type Result bool
 
-/*
-func (i *ipUpdater) ipupdate(args *Args, result *Result) error {
-	err := i.client.Call("ipupdater", args, &result)
-	return err
-}
-*/
 func main() {
 	var newipset []string
 	var oldipset []string
-
 	for {
 		//rand.Seed(time.Now().UTC().UnixNano())
 		url := "https://app.rainforestqa.com/api/1/vm_stack"
@@ -40,7 +25,6 @@ func main() {
 		//  compare
 		res := reflect.DeepEqual(newipset, oldipset)
 		if res == false {
-
 			client, err := net.Dial("tcp", string(os.Getenv("MACH1_ADDR")))
 			if err != nil {
 				fmt.Println("error : ", err)
@@ -51,16 +35,6 @@ func main() {
 			if err != nil {
 				log.Fatal("IpUpdateInit error:", err)
 			}
-			/*
-				for i := 0; i < len(newipset); i++ {
-					args := &Args{newipset[i]}
-					log.Println("send : ", args)
-					err = c.Call("IpUpdater.AccumIp", args, &res)
-					if err != nil {
-						log.Fatal("AccumIp error:", err)
-					}
-				}
-			*/
 			args := &Args{newipset}
 			err = c.Call("IpUpdater.IpUpdate", args, &res)
 			if err != nil {
@@ -93,65 +67,3 @@ func getIPsfromHTTP(url string) ([]string, bool) {
 	}
 	return data, true
 }
-
-/*
-func RandomIP() *ipSet {
-	var b, c, d, e int
-	totalNum := rand.Intn(10)
-	ipset := NewipSet(totalNum)
-
-	for i := 0; i < totalNum; i++ {
-		b, c, d, e = rand.Intn(500), rand.Intn(500), rand.Intn(500), rand.Intn(500)
-		ipstr := strconv.Itoa(b) + "." + strconv.Itoa(c) + "." + strconv.Itoa(d) + "." + strconv.Itoa(e)
-		ipset.ip[i] = ipstr
-		//fmt.Println(ipstr)
-	}
-
-	return ipset
-}
-
-func main() {
-	//newipset := ipSet{}
-	var newipset []string
-	//oldipset := ipSet{}
-	var oldipset []string
-	var homepath, path, cmd string
-	filename := "/addrbook.json"
-	for {
-		rand.Seed(time.Now().UTC().UnixNano())
-		//newipset = *RandomIP()
-		url := "https://app.rainforestqa.com/api/1/vm_stack"
-		newipset, _ = getIPsfromHTTP(url)
-		//  compare
-		res := reflect.DeepEqual(newipset, oldipset)
-		if res == false {
-			bolB, _ := ejson.Marshal(newipset)
-			home, _ := exec.Command("echo", os.Getenv("HOME")).Output()
-			homepath = strings.TrimSuffix(string(home), "\n")
-			path = homepath + filename
-			fmt.Println(path)
-			err := ioutil.WriteFile(path, bolB, 0644)
-			if err != nil {
-				errByte := []byte(err.Error())
-				ioutil.WriteFile("error.json", errByte, 0644)
-			}
-
-			// copy file to remote proxies
-			dest, _ := exec.Command("echo", os.Getenv("MACH1_ADDR")).Output()
-			cmd = "scp"
-			homepath = homepath + filename
-			result, err := exec.Command(cmd, homepath, string(dest)).Output()
-
-			fmt.Println("dest: ", dest, "result: ", result)
-			if err != nil {
-				fmt.Println("error : ", err)
-			}
-			fmt.Println(string(bolB))
-		}
-		oldipset = newipset
-		//sleep
-		time.Sleep(2000 * time.Millisecond)
-	}
-}
-
-*/
